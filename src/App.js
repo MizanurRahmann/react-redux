71c1f4121a309ef2e5f7eac38acea122cbf42e02
MizanurRahmann <mr926560@gmail.com>
2020-07-08 21:05:59 +0600

    Cakes and IceCream.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import store from './redux/cake/store'
+import store from './redux/store'
 import './App.css';
 import CakeContainer from './components/CakeContainer';
 import HooksCakeContainer from './components/HooksCakeContainer';
+import IceCreamContainer from './components/IceCreamContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        {/* <CakeContainer /> */}
-        <HooksCakeContainer />
+        <CakeContainer />
+        <IceCreamContainer />
+        {/* <HooksCakeContainer /> */}
       </div>
     </Provider>
   );
--- src/redux/cake/store.js
+++
@@ -1,8 +0,0 @@
-import {createStore} from 'redux';
-import cakeReducer from './cakeReducer';
-
-const store = createStore(cakeReducer);
-
-export default store;
-
-//provide the store to the react application
